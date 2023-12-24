856dfe33c8f5fa9afc62d14d75e9d5a0593f4289
Michael Duren <michaeld@michaelduren.com>
2023-12-24 10:58:14 -0600

    mobile view for card

--- src/routes/card/[slug]/+page.svelte
+++ src/routes/card/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Card } from '$lib/types/Card';
 	import { fly } from 'svelte/transition';
-	import { sineIn, sineOut } from 'svelte/easing';
+	import { sineIn } from 'svelte/easing';
 	export let data: { card: Card };
 	let open = false;
 
@@ -15,15 +15,9 @@
 	<meta name="card" content="personalized holiday card" />
 </svelte:head>
 
-<!-- add front card -->
-
 {#if !open}
 	<div class="card-front">
-		<div
-			in:fly={{ x: -200, duration: 1000 }}
-			out:fly={{ y: 200, duration: 1000, easing: sineOut }}
-			class="front-image-container shadow-xl"
-		>
+		<div out:fly={{ y: 200, duration: 1000 }} class="front-image-container shadow-xl">
 			<img src="/icons/Letter.png" alt="Christmas Tree" />
 		</div>
 	</div>
@@ -31,12 +25,17 @@
 	<div in:fly={{ delay: 1000, duration: 1500, y: 200, easing: sineIn }} class="card">
 		<div class="left-side shadow-xl">
 			<h1>{data.card.title}</h1>
+			<div class="left-image-container">
+				<img class="left-image" src="/icons/House.png" alt="House" />
+			</div>
 			<p>{data.card.content}</p>
-			<p>{data.card.signature}</p>
-			<p>Michael</p>
+			<p class="p-cursive">{data.card.signature}</p>
+			<p class="p-cursive">-Michael</p>
 		</div>
 		<div class="right-side shadow-xl">
-			<img src={data.card.image} alt={data.card.title} />
+			<div class="right-container">
+				<img src={data.card.image} alt={data.card.title} />
+			</div>
 		</div>
 	</div>
 {/if}
@@ -45,6 +44,12 @@
 	h1 {
 		font-family: var(--festive);
 		font-size: 3rem;
+		margin: 0;
+		padding: 0;
+	}
+	.p-cursive {
+		font-family: var(--festive);
+		font-size: 1.5rem;
 	}
 	.card-front {
 		display: flex;
@@ -58,7 +63,6 @@
 	.front-image-container {
 		width: 50%;
 		animation: fadeRight 1s ease-in-out;
-		flex: 1;
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -77,24 +81,59 @@
 	}
 	.card {
 		display: flex;
+		flex-direction: column;
 		flex: 1;
 		width: 100%;
 		animation: openCard 0.7s ease-in-out;
 	}
 	.left-side {
 		flex: 1;
-		border-radius: 1rem;
+		border-radius: 2rem;
+		border-top-right-radius: 0;
+		border-top-left-radius: 0;
 		background: #fff;
 		padding: 1rem;
 	}
+	.left-image {
+		width: 10rem;
+		object-fit: contain;
+	}
+	.left-image-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	.right-side {
+		border-radius: 2rem;
+		border-bottom-right-radius: 0;
+		border-bottom-left-radius: 0;
 		flex: 1;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		border-radius: 1rem;
 		background: linear-gradient(120deg, var(--red-200) 0%, var(--red-500) 27%, var(--red-800) 100%);
+	}
+	.right-container {
+		padding: 4rem;
+	}
+	@media (min-width: 768px) {
+		.card {
+			flex-direction: row;
+		}
+		.front-image-container {
+			flex: 1;
+		}
+		.left-side {
+			border-radius: 2rem;
+			border-top-left-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+		.right-side {
+			border-radius: 2rem;
+			border-top-right-radius: 0;
+			border-bottom-right-radius: 0;
+		}
 	}
 	img {
 		height: 100%;
